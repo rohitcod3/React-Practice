577c8c107cc5910a2923384317e76e52b8b459ea
rohitcod3 <rohit.bangar9@gmail.com>
2025-01-11 13:08:12 +0530

    create global context and added two components

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TodoProvider } from "./contexts/TodoContext";
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ todoContextLocal/src/components/TodoItem.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+const TodoItem = ({ todo }) => {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+  const { updateTodo, DeleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+  return (
+    <div
+      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${
+        todo.toggleCompleted ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+      }`}
+    >
+      <input
+        type="checkbox"
+        name=""
+        id=""
+        className="cursor-pointer"
+        checked={todo.toggleCompleted}
+        oncChange={toggleComplete}
+      />
+      <input
+        type="text"
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+        }`}
+        value={todoMsg}
+        onChange={(e) => setTodoMsg(e.target.value)}
+        readOnly={!isTodoEditable}
+      />
+      <button className="inline-flex w-8 h-8 rounded-lg text-sm border border-blacl/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50">
+        {isTodoEditable ? "save" : "edit"}
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
